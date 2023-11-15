da0cfda2fc442420784ae3ba7ef7cb4a17569c7d
DuartePonce <duarteponce4@gmail.com>
2023-11-15 19:06:39 +0000

    Basics

--- Project1/Ideias.md
+++ Project1/Ideias.md
@@ -43,13 +43,25 @@ int n = 2;
 
 std::list<std::list<int>> *listTypeSlabs = new std::list<std::list<int>>[n + 1];
 
-FIX nao da para ler as coisas como deve de ser
+FIXME nao da para ler as coisas como deve de ser 
+FIXME Dentro do primeiro for fazemos as operacoes de calculo de valor total
+FIXME Avaliar o metro por quadrado
 
-for (int i=1; i <= n; i++) {
+
+for (int i = 1; i <= n; i++) {
     std::cin >> a >> b >> p;
 
     std::list<int> listSpecs = {a, b, p};
 
     listTypeSlabs[i].push_front(listSpecs);
+}
+
+for (int i = 1; i <= n; i++) {
+    for (const auto &listSpecs : listTypeSlabs[i]) {
+        for (const auto &valueInt : listSpecs) {
+            std::cout << valueInt << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 ```
--- Project1/projeto1_test.cpp
+++ Project1/projeto1_test.cpp
@@ -6,16 +6,26 @@ int main() {
     int a, b, p;
     int n = 2;
 
-    std::list<int> *listS = new std::list<int>[n + 1];
+    std::list<std::list<int>> *listTypeSlabs = new std::list<std::list<int>>[n + 1];
 
-    for (int i=1; i <= n; i++) {
+    // FIX nao da para ler as coisas como deve de ser
+
+    for (int i = 1; i <= n; i++) {
         std::cin >> a >> b >> p;
 
         std::list<int> listSpecs = {a, b, p};
 
-    
+        listTypeSlabs[i].push_front(listSpecs);
     }
 
+    for (int i = 1; i <= n; i++) {
+        for (const auto &listSpecs : listTypeSlabs[i]) {
+            for (const auto &valueInt : listSpecs) {
+                std::cout << valueInt << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 
     return 0;
 }
