1509b67e5ec61b9eba5487897f98bace329cd249
Duarte Ponce <duarteponce4@gmail.com>
2023-11-18 23:53:12 +0000

    Primeiros passos

--- Project1/projeto1_test.cpp
+++ Project1/projeto1_test.cpp
@@ -2,30 +2,49 @@
 #include <list>
 #include <vector>
 
+typedef struct tile{
+    int x, y, price, quality; //Variable quality is only for testing for now i will work it out
+    struct tile* next;
+} tile;
+
+void createTile (tile* tile, int a, int b, int p, int q) { //Function that creates and gives the values of a tile
+    tile->x = a;
+    tile->y = b;
+    tile->price = p;
+    tile->quality = q;
+    tile->next = NULL;
+}
+
+void insertFuntion (tile** tileLinkedList, int a, int b, int p, int q) {
+    tile* tileList = *tileLinkedList;
+    tile* newTile = (tile*) malloc(sizeof(tile));
+    createTile(newTile, a, b, p, q);
+    if (tileList == NULL) { //Verifies if the linked list has anyting
+        *tileLinkedList = newTile;//creates the first element of the list
+    } else {
+        while(tileList != NULL && q != 0){//iteration to go through the list q is bulshit for now there
+            if(q >= tileList->quality) {//first step in wich i insert in the front
+                *tileLinkedList = newTile;
+                newTile->next = tileList;
+                q = 0;//bullshit
+            }
+
+            tileList = tileList->next;
+        }
+    }
+}
+
 int main() {
-    int a, b, p;
-    int n = 2;
-
-    std::list<std::list<int>> *listTypeSlabs = new std::list<std::list<int>>[n + 1];
-
-    // FIX nao da para ler as coisas como deve de ser
+    int x, y, n, a, b, p;
+    tile *tileLinkedList = NULL;
+    std::cin >> x >> y;
+    std::cin >> n;
 
     for (int i = 1; i <= n; i++) {
         std::cin >> a >> b >> p;
-
-        std::list<int> listSpecs = {a, b, p};
-
-        listTypeSlabs[i].push_front(listSpecs);
-    }
-
-    for (int i = 1; i <= n; i++) {
-        for (const auto &listSpecs : listTypeSlabs[i]) {
-            for (const auto &valueInt : listSpecs) {
-                std::cout << valueInt << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
+        int quality = i;
+        insertFuntion(&tileLinkedList, a, b, p, quality);
+    }   
 
     return 0;
 }
