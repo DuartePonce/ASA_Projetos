6c8d104cd8233b7c132cbf41d7421729f986daee
Duarte Ponce <duarteponce4@gmail.com>
2023-11-26 15:30:24 +0000

    yeah

--- Project1/proj1_new.cpp
+++ Project1/proj1_new.cpp
@@ -34,7 +34,7 @@ void Algorithm(std::vector<tile*>& tilesVector, int x, int y, int n) {
                         dp[i][j][k] = max ( j / tilesVector[i]->x * k / tilesVector[i]->y * tilesVector[i]->price, j / tilesVector[i]->y * k / tilesVector[i]->x * tilesVector[i]->price, 0, 0);
                     }
                     else {
-                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][x][k - tilesVector[i]->y + 1], dp[i - 1][tilesVector[i]->x][k - tilesVector[i]->y + 1], dp[i][j][k-1]);
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][x][k - tilesVector[i]->y + 1] + tilesVector[i]->price, dp[i - 1][tilesVector[i]->x][k - tilesVector[i]->y + 1] + tilesVector[i]->price, dp[i][j][k-1]);
                     }
                 }
                 else if (tilesVector[i]->x <= k && tilesVector[i]->y <= j) {
@@ -51,7 +51,7 @@ void Algorithm(std::vector<tile*>& tilesVector, int x, int y, int n) {
 
                 // else {
                 //     if ( tilesVector[i]->dim.y > x || tilesVector[i]->dim.y > x ) {
-                //         dp[i][j] = 0;
+                //         dp[i][j] = 0;    
                 //     }
                 // }
                 std::cout << dp[i][j][k] << "\n";
