f2e34d0d2a7aa5b08eb6b2c740287b56495d31cf
Duarte Ponce <duarteponce4@gmail.com>
2023-12-12 13:10:55 +0000

    Nem queria

--- Project2/firstfie.cpp
+++ Project2/firstfie.cpp
@@ -1,35 +1,109 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <vector>   
+#include <unordered_map>
 
-void grafo_esparso(int n, int m) {
-    std::vector<std::vector<int>> grafo(n + 1);
-    int v1, v2;
-    for (int i = 0; i <= m; ++i) {
-        scanf("%d %d", &v1, &v2);
-        grafo[v1].push_back(v2); 
+class Vertex {
+    public :
+        int id; // Number of the vertex
+        int color = 0; // 0 = white 1 = grey 2 = black;
+        int d = 0;
+        int f = 0;
+        int pi = 0;
+        std::vector<Vertex> adj; // List of vertex that the specific vertex points to
+    
+        // Constructors
+        Vertex() : id(0) {} // Default constructor
+
+        Vertex(int vertexId) : id(vertexId) {}
+        
+        void addEdge(int v1, int v2, std::unordered_map<int, Vertex>& vertexMap);
+
+};
+
+void Vertex::addEdge(int v1, int v2, std::unordered_map<int, Vertex>& vertexMap) {
+    // Check if v1 vertex exists, if not, create it
+    if (vertexMap.find(v1) == vertexMap.end()) {
+        vertexMap[v1] = Vertex(v1);
+    }
+    // Check if v2 vertex exists, if not, create it
+    if (vertexMap.find(v2) == vertexMap.end()) {
+        vertexMap[v2] = Vertex(v2);
+    }
+    // Now add the edge
+    vertexMap[v1].adj.push_back(vertexMap[v2]);
+}
+
+void printGraph(const std::unordered_map<int, Vertex>& vertexMap) {
+    for (const auto& entry : vertexMap) {
+        std::cout << "Vertex " << entry.first << " -> ";
+        for (const auto& adjVertex : entry.second.adj) {
+            std::cout << adjVertex.id << " ";
+        }
+        std::cout << std::endl;
     }
 }
-void grafo_denso(int n, int m) {
-    int v1, v2;
-    int** grafo = new int*[n + 1];
-    for (int i = 0; i < n + 1; ++i) {
-        grafo[i] = new int[n + 1];
+
+void DFS_Visit(std::unordered_map<int, Vertex>& vertexMap, Vertex& u, int& time) {
+    u.color = 1;
+    u.d = time;
+    time++;
+    for (int j = 0; j < (int) u.adj.size(); ++j) {
+        if (u.adj[j].color == 0) {
+            u.adj[j].pi = u.id;
+            DFS_Visit(vertexMap, u.adj[j], time);
+        }
     }
-    for (int i = 0; i < m; ++i) {
-        scanf("%d %d", &v1, &v2);
-        grafo[v1][v2] = 1;
+    u.color = 2;
+    u.f = time;
+    time++;
+    vertexMap[u.id] = u;
+}
+
+
+void DFS(std::unordered_map<int, Vertex>& vertexMap, std::vector<int>& roots) {
+    int time = 1;
+    for (int i = 0; i <= (int) roots.size(); ++i) {
+        auto it = vertexMap.find(roots[i]);
+        if (it != vertexMap.end()) {
+            
+            DFS_Visit(vertexMap, it->second, time);
+        }
     }
 }
-int main() {
-    int n, m;
+
+
+
+int main() { 
+    int n, m, v1, v2;
     scanf("%d %d", &n, &m);
 
-    double densidade = (2.0 * m) / (n * (n - 1));
-    if (densidade >= 0.5) { //Caso denso
-        grafo_denso(n, m);
-    } else {// Caso esparso
-        grafo_esparso(n, m);
+    std::unordered_map<int, Vertex> vertexMap;
+    vertexMap.reserve(n); // Reserving memory for n elements to prevent mmemory leaks
+
+
+    std::vector<int> roots(n);
+    for (int i = 0; i < n; ++i) {
+        roots[i] = i + 1;
     }
+    Vertex v(0);
+    for (int i = 0; i < m; ++i) {
+        scanf("%d %d", &v1, &v2);
+        v.addEdge(v1, v2, vertexMap);
+
+        auto it = std::find(roots.begin(), roots.end(), v2);
+        if (it != roots.end() || *it == v2) {
+            roots.erase(it);
+        }
+    }
+    printGraph(vertexMap);
+    DFS(vertexMap, roots);
+    int res = 0;
+    for (const auto& entry : vertexMap) {
+        res = std::max(res, entry.second.d);
+        
+    }
+    printf("%d", res);
+
     return 0;
 }
--- Project2/tester.cpp
+++ Project2/tester.cpp
@@ -2,24 +2,36 @@
 #include <algorithm>
 #include <vector>
 
+void grafo_esparso(int n, int m) {
+    std::vector<std::vector<int>> grafo(n + 1);
+    int v1, v2;
+    for (int i = 0; i <= m; ++i) {
+        scanf("%d %d", &v1, &v2);
+        grafo[v1].push_back(v2); 
+    }
+}
+
+void grafo_denso(int n, int m) {
+    int v1, v2;
+    int** grafo = new int*[n + 1];
+    for (int i = 0; i < n + 1; ++i) {
+        grafo[i] = new int[n + 1];
+    }
+    for (int i = 0; i < m; ++i) {
+        scanf("%d %d", &v1, &v2);
+        grafo[v1][v2] = 1;
+    }
+}
+
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    std::vector<std::vector<int>> grafo(n + 1); // Initialize graph
-
-    int v1, v2;
-    for (int i = 0; i <= m; ++i) {
-        std::cin >> v1 >> v2;
-        grafo[v1].push_back(v2); // Add edge v1->v2
-    }
-    printf("ola\n");
-    for (int i = 0; i <= n; ++i) {
-        printf("%d --->", i);
-        for (int j = 0; j <  (int) grafo[i].size(); ++j) { //int v : grafo[i]
-            printf("%d -  ", grafo[i][j]);
-        }
-        printf("\n");
+    double densidade = (2.0 * m) / (n * (n - 1));
+    if (densidade >= 0.5) { //Caso denso
+        grafo_denso(n, m);
+    } else {// Caso esparso
+        grafo_esparso(n, m);
     }
     return 0;
 }
