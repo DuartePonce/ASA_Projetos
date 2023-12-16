28e446a320ef50b1ff3a186075dffe9c4b81df38
Duarte Ponce <duarteponce4@gmail.com>
2023-12-16 19:01:44 +0000

    Die Irrae

--- Project2/tester.cpp
+++ Project2/tester.cpp
@@ -2,14 +2,27 @@
 #include <algorithm>
 #include <vector>
 
-int DFS_Visit() {
-    return 0;
+int DFS_Visit(std::vector<std::vector<int>> grafo, std::vector<int> colors, int n, int i) {
+    // a cena da cor é importante mas como?
+    int res = 0;
+    int prev = 0;
+    colors[i] = 1;
+    for (int j = 0; j < (int) grafo[i].size(); j++) {
+        if (colors[grafo[i][j]] == 0) {
+            prev = DFS_Visit(grafo, colors, n, grafo[i][j]);
+        }
+        res = std::max(res, 1 + prev);
+    }
+    return res;
 }
 
-void DFS(int** grafo, int n) {
+void DFS(std::vector<std::vector<int>> grafo, std::vector<int> colors, int n) {
     int res = 0;
+    // Maybe mudar os valores 1 para 0 e depois -1 para fechar ou assim ns bem
     for (int i = 1; i <= n; i++) {
-
+        if (colors[i] == 0) {
+            res = std::max(res, DFS_Visit(grafo, colors, n, i));
+        }
     }
     printf("%d\n", res);
 }
@@ -19,27 +32,19 @@ int main() {
     scanf("%d %d", &n, &m);
 
     int v1, v2;
-    int** grafo = new int*[n + 1];
-    for (int i = 0; i <= n; ++i) {
-        grafo[i] = new int[n + 1];
+    std::vector<std::vector<int>> grafo(n + 1);
+    std::vector<int> colors(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+        colors[i] = 0; // cor 0-Branco 1-Cinzento 2-Preto
     }
-    for (int i = 0; i <= m; ++i) {
+
+    for (int i = 0; i < m; ++i) {
         scanf("%d %d", &v1, &v2);
-        grafo[v1][v2] = 1;
+        grafo[v1].push_back(v2);
     }
+    
+    DFS(grafo, colors, n);
 
-
-
-    for (int i = 1; i <= n; i++) {
-        printf("%d -> ", i);
-        for ( int j = 1; j <= n; j++) {
-            printf("%d ", grafo[i][j]);
-        }
-        printf("\n");
-    }
-
-
-
-    DFS(grafo, n);
     return 0;
 }
