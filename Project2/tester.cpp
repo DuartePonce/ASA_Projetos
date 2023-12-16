ed42931125f0da738553608e282c0b72549a9cd6
Duarte Ponce <duarteponce4@gmail.com>
2023-12-16 15:21:21 +0000

    Die Irrae

--- Project2/tester.cpp
+++ Project2/tester.cpp
@@ -2,36 +2,44 @@
 #include <algorithm>
 #include <vector>
 
-void grafo_esparso(int n, int m) {
-    std::vector<std::vector<int>> grafo(n + 1);
-    int v1, v2;
-    for (int i = 0; i <= m; ++i) {
-        scanf("%d %d", &v1, &v2);
-        grafo[v1].push_back(v2); 
-    }
+int DFS_Visit() {
+    return 0;
 }
 
-void grafo_denso(int n, int m) {
-    int v1, v2;
-    int** grafo = new int*[n + 1];
-    for (int i = 0; i < n + 1; ++i) {
-        grafo[i] = new int[n + 1];
+void DFS(int** grafo, int n) {
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+
     }
-    for (int i = 0; i < m; ++i) {
-        scanf("%d %d", &v1, &v2);
-        grafo[v1][v2] = 1;
-    }
+    printf("%d\n", res);
 }
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    double densidade = (2.0 * m) / (n * (n - 1));
-    if (densidade >= 0.5) { //Caso denso
-        grafo_denso(n, m);
-    } else {// Caso esparso
-        grafo_esparso(n, m);
+    int v1, v2;
+    int** grafo = new int*[n + 1];
+    for (int i = 0; i <= n; ++i) {
+        grafo[i] = new int[n + 1];
     }
+    for (int i = 0; i <= m; ++i) {
+        scanf("%d %d", &v1, &v2);
+        grafo[v1][v2] = 1;
+    }
+
+
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d -> ", i);
+        for ( int j = 1; j <= n; j++) {
+            printf("%d ", grafo[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+    DFS(grafo, n);
     return 0;
 }
