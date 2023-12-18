a096131eee7f6b6abdc2f72e31512b99dca8fa64
Duarte Ponce <duarteponce4@gmail.com>
2023-12-18 22:38:10 +0000

    Legio Mortis

--- Project2/firstfie.cpp
+++ Project2/firstfie.cpp
@@ -4,51 +4,9 @@
 
 
 
-
-int DFS_Visit(std::vector<std::vector<int>> grafo, std::vector<int> colors, int i) {
-    // a cena da cor é importante mas como?
-    int res = 0;
-    int prev = 0;
-    colors[i] = 1;
-    for (int j = 0; j < (int) grafo[i].size(); j++) {
-        if (colors[grafo[i][j]] == 0) {
-            prev = DFS_Visit(grafo, colors, grafo[i][j]);
-        }
-        res = std::max(res, 1 + prev);
-    }
-    
-    return res;
-}
-
-void DFS(std::vector<std::vector<int>> grafo, std::vector<int> colors, int n) {
-    int res = 0;
-    // Maybe mudar os valores 1 para 0 e depois -1 para fechar ou assim ns bem
-    for (int i = 1; i <= n; i++) {
-        if (colors[i] == 0) {
-            res = std::max(res, DFS_Visit(grafo, colors, i));
-        }
-    }
-    printf("%d\n", res);
-}
-
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    int v1, v2;
-    std::vector<std::vector<int>> grafo(n + 1);
-    std::vector<int> colors(n + 1);
-
-    for (int i = 1; i <= n; ++i) {
-        colors[i] = 0; // cor 0-Branco 1-Cinzento 2-Preto
+    for (int i = 0; i < 10000; i++) {
+        printf("%d %d\n", i, i+1);
     }
-
-    for (int i = 0; i < m; ++i) {
-        scanf("%d %d", &v1, &v2);
-        grafo[v1].push_back(v2);
-    }
-    
-    DFS(grafo, colors, n);
-
     return 0;
 }
--- Project2/tester.cpp
+++ Project2/tester.cpp
@@ -4,30 +4,52 @@
 
 int SCC = 1;
 
+int DFS_Visit_final(std::vector<std::vector<int>>& grafo, std::vector<int>& colors, std::vector<int>& ft, int i) {
+    int son = 0;
+    colors[i] = 1;
+    for (int j = 0; j < (int) grafo[i].size(); j++) {
+        if (colors[grafo[i][j]] == 0) {
+            son = DFS_Visit_final(grafo, colors, ft, grafo[i][j]);
+        }
+        ft[i] = std::max(ft[i], 1 + son);
+    }
+
+    return ft[i];
+}
+
+void DFS_final(std::vector<std::vector<int>>& grafo, std::vector<int>& colors, std::vector<int>& ft, int n) {
+
+    for (int i = 0; i <= n; i++) {  // Adjusted loop to start from index 0
+        if (colors[i] == 0) {
+            DFS_Visit_final(grafo, colors, ft, i);
+        }
+    }
+}
+
 
 void DFS_visit_SCC(std::vector<std::vector<int>>& grafo, std::vector<int>& colors, int i, std::vector<std::vector<int>>& grafo_final, std::vector<int>& stack_SCC) {
     colors[i] = 1;
 
     for (int j = 0; j < (int)grafo[i].size(); j++) {
-        if (colors[grafo[i][j]] == 0 && stack_SCC[i] != stack_SCC[grafo[i][j]]) {
-            printf("color: %d - stack[i]: %d - stacj[grafo]: %d\n",colors[i], stack_SCC[i], stack_SCC[grafo[i][j]]);
-            grafo_final[stack_SCC[i]].push_back(stack_SCC[grafo[i][j]]);
+        if (stack_SCC[i] != stack_SCC[grafo[i][j]]) {
+            int adj = stack_SCC[grafo[i][j]];
+            grafo_final[stack_SCC[i]].push_back(adj);
         }
     }   
 }
 
-void DFS_SCC(std::vector<std::vector<int>>& grafo, std::vector<int>& colors, int n, std::vector<std::vector<int>>& grafo_final, std::vector<int>& stack_SCC) {
+void DFS_SCC(std::vector<std::vector<int>>& grafo, int n, std::vector<std::vector<int>>& grafo_final, std::vector<int>& stack_SCC) {
+    std::vector<int> colors(n + 1, 0);
 
 
     for (int i = 1; i <= n; i++) {
         if (colors[i] == 0) {
-            printf("i : %d\n", i);
             DFS_visit_SCC(grafo, colors, i, grafo_final, stack_SCC);
         }
     }
 }
 
-void DFS_Visit_transposta(std::vector<std::vector<int>> grafo, std::vector<int>& colors, int i, std::vector<int>& stack_SCC) {
+void DFS_Visit_transposta(std::vector<std::vector<int>>& grafo, std::vector<int>& colors, int i, std::vector<int>& stack_SCC) {
     colors[i] = 1;
     for (int j = 0; j < (int) grafo[i].size(); j++) {
         if (colors[grafo[i][j]] == 0) {
@@ -38,8 +60,8 @@ void DFS_Visit_transposta(std::vector<std::vector<int>> grafo, std::vector<int>&
     }
 }
 
-void DFS_transposta(std::vector<std::vector<int>> grafo, std::vector<int>& colors, int n, std::vector<int>& priority_list, std::vector<int>& stack_SCC) {
-
+void DFS_transposta(std::vector<std::vector<int>>& grafo, int n, std::vector<int>& priority_list, std::vector<int>& stack_SCC) {
+    std::vector<int> colors(n + 1, 0);
     for (int i = 0; i < n; i++) {
         if (colors[priority_list[i]] == 0) {
             DFS_Visit_transposta(grafo, colors, priority_list[i], stack_SCC);
@@ -64,7 +86,8 @@ int DFS_Visit(std::vector<std::vector<int>> grafo, std::vector<int>& colors, std
     return ft[i];
 }
 
-void DFS(std::vector<std::vector<int>> grafo, std::vector<int>& colors, std::vector<int>& ft, int n, std::vector<int>& priority_list) {
+void DFS(std::vector<std::vector<int>> grafo, std::vector<int>& ft, int n, std::vector<int>& priority_list) {
+    std::vector<int> colors(n + 1, 0);
 
     for (int i = 1; i <= n; i++) {
         if (colors[i] == 0) {
@@ -76,53 +99,45 @@ void DFS(std::vector<std::vector<int>> grafo, std::vector<int>& colors, std::vec
 
 int main() {
     int n, m;
+    int v1, v2;
     scanf("%d %d", &n, &m);
 
-    int v1, v2;
     std::vector<std::vector<int>> grafo(n + 1);
     std::vector<std::vector<int>> transposto(n + 1);
     std::vector<int> stack_SCC(n+1);
-    std::vector<int> colors(n + 1);
     std::vector<int> ft(n + 1);            
+    std::vector<int> ft2(n + 1); 
+    std::vector<int> colors(n + 1);
     std::vector<int> priority_list(n);
 
-    for (int i = 1; i <= n; ++i) {
-        colors[i] = 0; // cor 0-Branco 1-Cinzento 2-Preto
-        ft[i] = 0;
-    }
 
     for (int i = 0; i < m; ++i) {
         scanf("%d %d", &v1, &v2);
         grafo[v1].push_back(v2);
         transposto[v2].push_back(v1);
     }
-    
-    DFS(grafo, colors, ft, n, priority_list);
-    std::fill(colors.begin(), colors.end(), 0);
+    for (int i = 0; i <= n; i++) {
+        ft[i] = 0;
+        ft2[i] = 0;
+        colors[i] = 0;
+    }
+    DFS(grafo, ft, n, priority_list);
 
-    DFS_transposta(transposto, colors, n, priority_list, stack_SCC);
-    std::fill(colors.begin(), colors.end(), 0);
-
+    printf("1\n");
+    DFS_transposta(transposto, n, priority_list, stack_SCC);
+    printf("2\n");
     std::vector<std::vector<int>> grafo_final(SCC);
 
-    DFS_SCC(grafo, colors, n, grafo_final, stack_SCC);
+    DFS_SCC(grafo, n, grafo_final, stack_SCC);
 
-    for (int i  = 0 ; i < (int) grafo_final.size(); i++) {
-        printf("%d -> ", i);
-        for (int j = 0 ; j < (int) grafo_final[i].size(); j++) {
-            printf("%d ", j);
-        }
-        printf("\n");
-    }
-    std::fill(colors.begin(), colors.end(), 0);
-    std::fill(ft.begin(), ft.end(), 0);
+    printf("3\n");
 
-    printf("ola\n");
-    DFS(grafo_final, colors, ft, n, priority_list);
 
+    DFS_final(grafo_final, colors, ft2, n);
+    printf("4\n");
     int res = 0;
     for (int i = 1; i <= n; ++i) {
-        res = std::max(res, ft[i]);
+        res = std::max(res, ft2[i]);
     }
     printf("%d\n",res);
 
