9a6d583f2b9074c84298bb0b629f621b7d065ba1
Duarte Ponce <duarteponce4@gmail.com>
2023-12-20 18:49:50 +0000

    Cicatrix Malidictum

--- Project2/finalProj.cpp
+++ Project2/finalProj.cpp
@@ -63,7 +63,7 @@ void DFS_transposta(std::vector<std::vector<int>>& grafo, int n, std::vector<int
     std::vector<int> colors(n + 1, 0); // 0 = white, 1 = gray, 2 = black
     std::stack<int> stack;
 
-    for (int i = 0; i < (int) priority_list.size(); ++i) {
+    for (int i = (int) priority_list.size() - 1; i > 0; --i) {
         if (colors[priority_list[i]] == 0) {
             stack.push(priority_list[i]);
             while (!stack.empty()) {
@@ -107,7 +107,7 @@ void DFS(std::vector<std::vector<int>>& grafo, int n, std::vector<int>& priority
                     }
                 } else {
                     if (colors[current] == 1) {
-                        priority_list.insert(priority_list.begin(), current);
+                        priority_list.push_back(current);
                     }
                     colors[current] = 2;
                     stack.pop();
@@ -139,29 +139,15 @@ int main() {
         transposto[v2].push_back(v1);
     }
 
-    auto start = std::chrono::high_resolution_clock::now();
     DFS(grafo, n, priority_list);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "DFS elapsed time: " << duration.count()*1000 << " seconds" << std::endl;
 
-    auto start2 = std::chrono::high_resolution_clock::now();
     DFS_transposta(transposto, n, priority_list, stack_SCC);
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration2 = end2 - start2;
-    std::cout << "DFS transposta elapsed time: " << duration2.count()*1000 << " seconds" << std::endl;
+  
     std::vector<std::vector<int>> grafo_final(SCC);
 
-    auto start3 = std::chrono::high_resolution_clock::now();
     SCC_builder(grafo, n, grafo_final, stack_SCC);
-    auto end3 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration3 = end3 - start3;
-    std::cout << "DFS builder elapsed time: " << duration3.count()*1000 << " seconds" << std::endl;
 
-    auto start4 = std::chrono::high_resolution_clock::now();
     DFS_final(grafo_final, stack_SCC);
-    auto end4 = std::chrono::high_resolution_clock::now();   
-    std::chrono::duration<double> duration4 = end4 - start4;
-    std::cout << "DFS final elapsed time: " << duration4.count()*1000 << " seconds" << std::endl;
+
     return 0;
 }
